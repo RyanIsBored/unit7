5d775bfe2190da9546d44f934096abfba295d359
RyanIsBored <35492798+RyanIsBored@users.noreply.github.com>
2018-05-24 08:49:53 -0400

    Updated from Brython Server: 5/24/2018 8:49:52 AM

--- battleship.py
+++ battleship.py
@@ -1,3 +1,16 @@
 #Ryan Jones
 #5/23/18
 
+def printBoard(board):
+    for r in range(0,5):
+        for c in range(0,5):
+            print(board[r][c],end=' ')
+        print()
+
+board = [['0','0','0'],['0','0','0'],['g','h','i']]
+printBoard(board)
+
+row = int(input('Enter a row: '))
+col = int(input('Enter a column: '))
+board[row-1][col-1] = 'X'
+printBoard(board)
